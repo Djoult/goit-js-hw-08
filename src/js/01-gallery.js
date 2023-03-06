248e89cb75faa403db87739b144ca8ff8e91d846
Djoult <55453354+Djoult@users.noreply.github.com>
2023-03-06 12:41:29 +0200

    Changing gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+  <a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>`
+  )
+  .join('');
+let lightbox = new SimpleLightbox('.gallery a', {
+  navText: ['&#11178;', '&#11179;'],
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionClass: '',
+  //   closeText: "&#10015;", краще не розкоментовувати :)
+  closeText: '&#10803',
+  showCounter: false,
+});
