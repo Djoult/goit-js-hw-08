70d06f229ab0c2f8bf21e929b73fc79ec76bf8bf
Djoult <55453354+Djoult@users.noreply.github.com>
2023-03-07 11:28:02 +0200

    Solving task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+handlePageRestart();
+
+const handleTimeUpdate = function (data) {
+  console.log(data.seconds);
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+  // data is an object containing properties specific to that event
+};
+
+function handlePageRestart() {
+  const currentTime = localStorage.getItem(CURRENT_TIME);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
+
+player.on('timeupdate', _throttle(handleTimeUpdate, 1000));
